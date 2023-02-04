bb2df0f35fba59f3262700b709713effeff9eca3
HereIsAbhi <kabhinav201412@gmail.com>
2023-02-04 22:50:56 +0530

    basics

---
+++ p1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+//	int a= 15;
+//	float p= 2*3.14*a;
+//	int p1= 2*3.14*a;
+//	printf("%f \n",p);
+//	printf("%d",p1);
+//	char star= '*';
+//	printf("%c",star);
+//	int age;
+//	printf("enter age: ");
+//	scanf("%d", &age);
+//	printf("age is %d \n",age);
+	int a,b;
+	printf("enter a:");
+	scanf("%d", &a)	;
+	printf("enter b:");
+	scanf("%d", &b);
+	int sum= a+b;
+	printf("sum of a and b is:\n %d",sum);
+}
