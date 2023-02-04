91c8fc99c1984ef6aa0c84f1b570f14056257fe6
HereIsAbhi <kabhinav201412@gmail.com>
2023-02-04 22:52:29 +0530

    basic

---
+++ p2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+//	int side;
+//	printf("enter side:");
+//	scanf("%d", &side);
+//	printf("area of sq is %d",side*side);
+//	int r;
+//	float pi= 3.14;
+//	printf("enter radius:");
+//	scanf("%d", &r);
+//	int area= pi*r*r;
+//	printf("area is %d",area);
+	char a= 'a';
+	printf("character is %c",a);
+	
+}
---
+++ p3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+//	int a,b;
+//	printf("enter a:");
+//	scanf("%d",&a);
+//	printf("enter b:");
+//	scanf("%d",&b);
+//	printf("perimeter of rectangle is : %d",2*(a+b));
+	/*int a;
+	printf("enter a: ");
+	scanf("%d",&a);
+	printf("cube of a: %d",a*a*a);*/
+	int a,b,c;
+	printf("enter a:")
+	scanf("%d", &a);
+	printf("enter b:")
+	scanf("")
+}
