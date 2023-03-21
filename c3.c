776e2bed1f53c7481438004643ab38f20a424421
HereIsAbhi <kabhinav201412@gmail.com>
2023-03-21 12:25:45 +0530

    ifelse

---
+++ c3.c
@@ -0,0 +1,87 @@
+//switch case..................
+
+#include<stdio.h>
+int main(){
+
+ int a;
+ scanf("%d",&a);
+ switch(a)
+	{
+			
+		case 1:
+			printf("jan");
+			break;
+		case 2:
+			printf("feb");
+			break;	
+		case 3:
+			printf("march");
+			break;
+		
+		case 4:
+			printf("apr");
+			break;	
+		case 5:
+			printf("may");
+			break;	
+		case 6:
+			printf("june");
+			break;	
+		case 7:
+			printf("july");
+			break;
+		case 8:
+			printf("aug");
+			break;
+		case 9:
+			printf("sept");
+			break;	
+		case 10:
+			printf("oct");
+			break;
+		case 11:
+			printf("nov");
+			break;
+		case 12:
+			printf("dec");
+		default :
+			printf("invalid");
+			break;	
+								
+	
+//	int a,b;
+//	char c;
+//	printf("enter operation:");
+//	scanf("%c",&c);
+//	
+//	printf("enter a:");
+//	scanf("%d",&a);
+//	printf("enter b:");
+//	scanf("%d\n",&b);
+//	
+//	
+//	switch(c)
+//	{
+//		case '+':
+//			printf("%d+%d=%d",a,b,a+b);
+//			break;
+//		case '-':
+//			printf("%d-%d=%d",a,b,a-b);
+//			break;
+//		case '*':
+//			printf("%d*%d=%d",a,b,a*b);
+//			break;
+//		case '/':
+//			printf("%d/%d=%d",a,b,a/b);
+//			break;
+//		case '%':
+//			printf("%d%%d=%d",a,b,a%b);
+//		default:
+//			printf("invalid");	
+//					
+//
+//			
+//		 
+//		
+	}
+}
