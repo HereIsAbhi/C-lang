060a511c34deab9b6ba2567e1304747afdba7180
HereIsAbhi <kabhinav201412@gmail.com>
2023-03-21 12:30:07 +0530

    loops

---
+++ c4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+//	int n=10;
+//	 while(n>0){
+//		printf("%d\n",n);
+//		n=n-1;
+//	 }
+//	int a=10;
+//	while(a>0){
+//		a=a-1;
+//		printf("%d\n",a);
+//	}
+	int n;
+	int a=1;
+	printf("enter no:")
+	scanf("%d",&n);
+	while(a<=10){
+		printf("%d*%d=%d\n",n,a,a*n);
+		a=a+1;
+	}
+
+//	int n,sum,i=1;
+//	scanf("%d",&n);
+//	while(i<n){
+//		if(n%i==0){
+//			sum=0;
+//			sum+=i;
+//		}
+//		i=i+1;
+//		
+//	}
+//	if(sum==n){
+//		printf("perfect");
+//	}
+//	else{
+//		printf("non perfect");
+//	}
+}
