87ea3b52e9d2d762a32af94c72d109769e3daf3a
HereIsAbhi <kabhinav201412@gmail.com>
2023-03-23 21:30:07 +0530

    c lang

---
+++ c1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+//	double a,b;
+//	printf("enter a:");
+//	scanf("%f", &a);
+//	printf("%d",sizeof(a));
+//	printf("enter b:");
+//	scanf("%d", &b);
+//	printf("%d", a>=b);
+//	printf("sum is %d \n",a+b);
+//	printf("sub is %d \n",a-b);
+//	printf("mult is %d \n",a*b);
+//	printf("div is %d ",a/b);
+//	printf("mod is %d ",a%b);
+//	char a= 'hello';
+//	printf("%s",a);
+	int a,b;
+	a=9;b=5;
+	a+=b;
+	b= a-b;
+	a= a-b;
+	
+	printf("%d \n",a);
+	printf("%d",b);
+	
+	
+}
