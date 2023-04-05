99d7e1b9afa16d8b8fa740324f5f0160fc3e4f63
HereIsAbhi <kabhinav201412@gmail.com>
2023-04-05 22:25:24 +0530

    practice

---
+++ sum2343.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+//	int n,prod=1,i=1;
+//	scanf("%d",&n);
+//	while(i<=n){
+//		prod*=i;
+//		i++;
+//	}
+//	printf("%d",prod);
+	double i,n,prod;
+	scanf("%lf",&n);
+	for(i=1;i<=n;i++){
+		prod*=i;
+		
+	}
+//	printf("%d",prod);
+//	int i=10;
+//	do {
+//		printf("%d",i);
+//		i++;
+//	}while(i<10);
+}
